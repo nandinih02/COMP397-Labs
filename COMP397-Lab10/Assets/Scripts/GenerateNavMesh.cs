acc91d06ef97080335f9848c45d3fb1abb5063de
Nandini <juicyfruit5600@yahoo.ca>
2022-03-26 02:30:30 -0400

    Added Lab 10 files

---
+++ COMP397-Lab10/Assets/Scripts/GenerateNavMesh.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.AI.Navigation;
+public class GenerateNavMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ COMP397-Lab10/Assets/Scripts/WorldMaker.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMaker : MonoBehaviour
+{
+    [Header("Player Properties")]
+     
+    [Header("World Properties")]
+    [Range(1,16)]
+    public int height=1;
+    [Range(1, 64)]
+    public int width=1;
+    [Range(1, 64)]
+    public int depth=1;
+
+    [Header("Tile Properties")]
+    public GameObject threeDTile;
+    public Transform tileParent;
+
+    [Header("Scaling Values")]
+    public float min = 16.0f;
+    public float max = 24.0f;
+
+    [Header("Grid")]
+    public List<GameObject> grid;
+
+    private int startHeight;
+    private int startWidth;
+    private int startDepth;
+    private float startMin;
+    private float startMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new List<GameObject>();
+
+        Initialize();
+        Reset();
+        Regenerate();
+    }
+
+    void Update()
+    {
+        if(height != startHeight || depth != startDepth || width != startWidth || min != startMin || max != startMax)
+        {
+            Initialize();
+            Reset();
+            Regenerate();
+            
+        }
+    }
+
+    private void Initialize()
+    {
+        startHeight = height;
+        startWidth = width;
+        startDepth = depth;
+        startMin = min;
+        startMax = max;
+    }
+
+    private void Regenerate()
+    {
+        float rand = Random.Range(min, max);
+
+        float offsetX = Random.Range(-1024.0f, 1024.0f);
+        float offsetZ = Random.Range(-1024.0f, 1024.0f);
+
+        for (int y = 0; y < height; y++)
+        {
+
+            for (int z = 0; z < depth; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (y < Mathf.PerlinNoise((x + offsetX) / rand, (z + offsetZ) / rand) * depth * 0.5f)
+                    {
+                        var tile = Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                        tile.transform.parent = tileParent;
+                        grid.Add(tile);
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        foreach(var tile in grid)
+        {
+            Destroy(tile);
+        }
+        grid.Clear();
+    }
+}
